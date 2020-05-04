6c59ee569fa4ca341a8d04a9ae7184af63f154c1
unknown <dan-santiago@live.com>
2020-05-03 22:32:03 -0400

    Alterações para o uso do React

--- backend/src/app/controllers/ScheduleController.js
+++ backend/src/app/controllers/ScheduleController.js
@@ -27,6 +27,13 @@ class ScheduleController {
           ],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
 
